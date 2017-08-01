4b875c8030673dfcd3e81078922980b96504bcc6
Henning Støverud <henning@stoverud.no>
2017-08-01 22:33:21 +0200

    Create BlogController.cs

---
+++ Site/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Site.Controllers
+{
+    public class BlogController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect("http://stoverud.no");
+        }
+    }
+}
